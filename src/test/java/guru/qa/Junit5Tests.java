1c510e9ee60aaced2f48cb6ebf634cf5ab80fef5
Daniil <9414354@mail.ru>
2022-04-05 14:59:39 +0300

    small refactor & added logger slf4j

--- src/test/java/guru/qa/Junit5Tests.java
+++ src/test/java/guru/qa/Junit5Tests.java
@@ -3,7 +3,7 @@ package guru.qa;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 
-public class SimpleTest {
+public class Junit5Tests {
 
     @BeforeAll
     static void initDB() {
